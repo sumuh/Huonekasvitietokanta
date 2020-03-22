f1aa2cd41bb96b111dec27fba41e852d53158177
Susanna Muhli <muhli.susanna@gmail.com>
2020-03-22 18:37:00 +0200

    database test

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from application import app
 from application.auth.models import User
@@ -23,4 +23,4 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return redirect(url_for("index"))   
+    return redirect(url_for("index"))
