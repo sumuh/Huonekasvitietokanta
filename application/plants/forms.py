c59d97f10edd172c89a33fc0cffcb4fae45bdfff
Susanna Muhli <muhli.susanna@gmail.com>
2020-04-12 13:22:34 +0300

    categories can now be edited and deleted

--- application/plants/forms.py
+++ application/plants/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SelectMultipleField, validators
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from wtforms import StringField, SelectField, SelectMultipleField, validators, RadioField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
 
 from application.auth.models import User
 from .models import Plant, PlantUser, Category, PlantCategory
@@ -20,6 +20,7 @@ class PlantForm(FlaskForm):
         "Valontarve",
         choices=[('Pieni', 'Pieni'), ('Kohtalainen', 'Kohtalainen'), ('Suuri', 'Suuri')]
     )
+    #kategoriat = QuerySelectMultipleField("Kategoriat", query_factory=Category.category_query, get_label="nimi")
     class Meta:
         csrf = False
 
@@ -28,7 +29,7 @@ class SearchForm(FlaskForm):
     class Meta:
         csrf = False
 
-class CreateCategoryForm(FlaskForm):
+class CategoryForm(FlaskForm):
     nimi = StringField("Kategorian nimi", [validators.Length(min=2, message="Nimen tulee olla vähintään 2 merkkiä pitkä")])
     kuvaus = StringField("Kategorian kuvaus", [validators.Length(min=2, message="Kuvauksen tulee olla vähintään 2 merkkiä pitkä")])
     class Meta:
--- application/plants/views.py
+++ application/plants/views.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 
 from application import app, db
 from application.plants.models import Plant, PlantUser, Category, PlantCategory
-from application.plants.forms import PlantForm, SearchForm, CreateCategoryForm, SearchCategoryForm
+from application.plants.forms import PlantForm, SearchForm, CategoryForm, SearchCategoryForm
 from application.auth.models import User
 
 from sqlalchemy.sql import text
@@ -107,9 +107,9 @@ def plants_delete(plant_id):
 
     return redirect(url_for("plants_show_all"))
 
-@app.route("/new/connection/<plant_id>", methods=["POST"])
+@app.route("/new/user/connection/<plant_id>", methods=["POST"])
 @login_required
-def plants_new_connection(plant_id):
+def plants_new_user_connection(plant_id):
 
     p = Plant.query.get(plant_id)
     user = User.query.get(current_user.id)
@@ -123,9 +123,9 @@ def plants_new_connection(plant_id):
 
     return redirect(url_for("plants_show_all"))
 
-@app.route("/delete/connection/<plant_id>", methods=['POST'])
+@app.route("/delete/user/connection/<plant_id>", methods=['POST'])
 @login_required
-def plants_delete_connection(plant_id):
+def plants_delete_user_connection(plant_id):
 
     p = Plant.query.get(plant_id)
     pu = PlantUser.query.filter_by(user=current_user, plant=p).first()
@@ -176,12 +176,12 @@ def categories_search():
 @app.route("/new/category/", methods=['GET'])
 @login_required
 def categories_new_form():
-    return render_template("categories/new.html", form = CreateCategoryForm(), kategoriat=Category.query.all())
+    return render_template("categories/new.html", form = CategoryForm(), kategoriat=Category.query.all())
 
 @app.route("/new/category/", methods=['POST'])
 def categories_new():
 
-    form = CreateCategoryForm(request.form)
+    form = CategoryForm(request.form)
 
     if not form.validate():
         return render_template("categories/new.html", form = form, kategoriat=Category.query.all())
@@ -198,4 +198,64 @@ def categories_new():
     db.session().add(c)
     db.session().commit()
 
+    return redirect(url_for("categories_new"))
+
+@app.route("/new/category/connection/<plant_id>", methods=["POST"])
+@login_required
+def categories_new_plant_connection(plant_id):
+
+    p = Plant.query.get(plant_id)
+    c = Category.query.get(current_user.id)
+
+    connectionExists = PlantUser.query.filter_by(user=user, plant=p).first()
+    if not connectionExists:
+        pu = PlantUser(plant=p, user=user)
+
+        db.session().add(pu)
+        db.session().commit()
+
     return redirect(url_for("plants_show_all"))
+
+@app.route("/manage/categories/", methods=['GET'])
+@login_required
+def categories_manage():
+    return render_template("/categories/manage.html", kategoriat=Category.query.all())
+
+@app.route("/manage/categories/update/<category_id>", methods=['GET'])
+@login_required
+def categories_update_form(category_id):
+    c = Category.query.get(category_id)
+    return render_template("/categories/update.html", category_id = category_id, form = CategoryForm(obj=c))
+
+@app.route("/manage/categories/update/<category_id>", methods=['POST'])
+@login_required
+def categories_update(category_id):
+
+    c = Category.query.get(category_id)
+    form = CategoryForm(request.form)
+
+    if not form.validate():
+        return render_template("categories/update.html", category_id = plant_id, form = form)
+
+    c.nimi = form.nimi.data
+    c.kuvaus = form.kuvaus.data
+
+    db.session().commit()
+
+    return redirect(url_for("categories_manage"))
+
+@app.route("/delete/category/<category_id>")
+@login_required
+def categories_delete(category_id):
+
+    c = Category.query.get(category_id)
+    pc = PlantCategory.query.all()
+    for i in pc:
+        if i.category_id == category_id:
+            db.session.delete(i)
+            db.session.commit()
+
+    db.session.delete(c)
+    db.session.commit()
+
+    return redirect(url_for("categories_manage"))
